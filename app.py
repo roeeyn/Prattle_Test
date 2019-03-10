711adb3552c1ff8e54e8a54257b9a956f325e933
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2019-03-10 14:22:43 -0600

    Refactorized soup function

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from functional import seq
 
 from lib.archive import get_archive_list
 from lib.posts import get_posts_from_year, get_post_text
-from lib.soup import get_soup_from_link
+from lib.soup import get_soup_from_link, get_description_soup
 
-def get_description_soup(post):
-  soup = get_soup_from_link(post.get('link'))
-  post['description'] = soup
-  return post
+# def get_description_soup(post):
+#   soup = get_soup_from_link(post.get('link'))
+#   post['description'] = soup
+#   return post
 
 def add_description_to_dict(year_posts):
   return (seq(year_posts)
@@ -22,12 +22,12 @@ fb_page = 'https://newsroom.fb.com/news'
 page = urlopen(fb_page)
 soup = BeautifulSoup(page, 'html.parser')
 
-archive_list_links = (seq(get_archive_list(soup))
+info_dictionary = (seq(get_archive_list(soup))
   .map(lambda link_dict: get_soup_from_link(link_dict.get('link'))) # Sirve que la consulta es más sencilla y puedes filtrar por fecha
   .map(lambda soup: get_posts_from_year(soup))
   .map(lambda post: add_description_to_dict(post)))
 
-for cosa in archive_list_links:
+for cosa in info_dictionary:
   print('-'*20)
   print(cosa)
   print('-'*20)
--- lib/soup.py
+++ lib/soup.py
@@ -4,3 +4,8 @@ from bs4 import BeautifulSoup
 def get_soup_from_link(link):
   page = urlopen(link)
   return BeautifulSoup(page, 'html.parser')
+
+def get_description_soup(post):
+  soup = get_soup_from_link(post.get('link'))
+  post['description'] = soup
+  return post
