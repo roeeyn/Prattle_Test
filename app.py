8fb1ac7e88c675304d34ae67453708a5e4dbce40
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2019-03-10 18:43:04 -0600

    Added filter for posts dates

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# Título, fecha, link y texto completo de la nota
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from functional import seq
 import pandas as pd
 
 from lib.archive import get_archive_list
-from lib.posts import get_posts_from_year, get_post_text, add_description_to_dict, filter_link_by_year
+from lib.posts import get_posts_from_year, get_post_text, add_description_to_dict, filter_link_by_year, filter_post_by_date
 from lib.soup import get_soup_from_link, get_description_soup
 
 fb_page = 'https://newsroom.fb.com/news'
@@ -13,16 +12,18 @@ fb_page = 'https://newsroom.fb.com/news'
 page = urlopen(fb_page)
 soup = BeautifulSoup(page, 'html.parser')
 
-# Date Format - December 15, 2013
-initialDate = { "day": 1, "month": 12, "year": 2017 }
+####### FILTER DATES ##########
+initialDate = { "day": 31, "month": 12, "year": 2017 }
 endingDate = { "day": 1, "month": 12, "year": 2019 }
+###############################
 
 print('Fetching data...')
 info_dictionary = (seq(get_archive_list(soup)) # Bring all years available in 'Archivo' section
-  .filter(lambda link: filter_link_by_year(link.get('link'), initialDate, endingDate))
+  .filter(lambda link: filter_link_by_year(link.get('link'), initialDate, endingDate)) # First filter, only by year
   .map(lambda link_dict: get_soup_from_link(link_dict.get('link'))) # While we fetch all years, it's better for filtering the years
-  .map(lambda soup: get_posts_from_year(soup))
-  .flat_map(lambda post: add_description_to_dict(post)))
+  .flat_map(lambda soup: get_posts_from_year(soup))
+  .filter(lambda post: filter_post_by_date(post, initialDate, endingDate)) # Filter by complete date before bringing the description
+  .map(lambda post: add_description_to_dict(post)))
 print('Finished fetching data')
 
 print('Started to create data Frame...')
--- lib/posts.py
+++ lib/posts.py
@@ -1,9 +1,42 @@
 from functional import seq
 from lib.soup import get_description_soup
+from datetime import datetime
 
-def filter_link_by_year(link, initialDate, endingDate):
+def get_month_number_from_string(string_month):
+  return (
+    1 if string_month == 'january'
+    else 2 if string_month == 'february'
+    else 3 if string_month == 'march'
+    else 4 if string_month == 'april'
+    else 5 if string_month == 'may'
+    else 6 if string_month == 'june'
+    else 7 if string_month == 'july'
+    else 8 if string_month == 'august'
+    else 9 if string_month == 'september'
+    else 10 if string_month == 'october'
+    else 11 if string_month == 'november'
+    else 12 if string_month == 'december'
+    else -1)
+
+def get_date_dict_from_post(post):
+  # Date Format - December 15, 2013
+  processed_date = post["date"].replace(',', '').split()
+  string_month = processed_date[0].lower()
+  month_number = get_month_number_from_string(string_month)
+  day = int(processed_date[1])
+  year = int(processed_date[2])
+  return { "day": day, "month": month_number, "year": year }
+
+def filter_post_by_date(post, initial_date, ending_date):
+  post_date_dict = get_date_dict_from_post(post)
+  initial_datetime = datetime(initial_date["year"], initial_date["month"], initial_date["day"])
+  ending_datetime = datetime(ending_date["year"], ending_date["month"], ending_date["day"])
+  post_datetime = datetime(post_date_dict["year"], post_date_dict["month"], post_date_dict["day"])
+  return initial_datetime <= post_datetime <= ending_datetime
+
+def filter_link_by_year(link, initial_date, ending_date):
   link_year = int(link[-4:])
-  return initialDate["year"] <= link_year <= endingDate["year"]
+  return initial_date["year"] <= link_year <= ending_date["year"]
 
 def get_post_text(post):
   post['description'] = post.get('description').find('div', class_='post-content')
@@ -23,6 +56,5 @@ def get_posts_from_year(soup):
   return posts_info
 
 def add_description_to_dict(year_posts):
-  return (seq(year_posts)
-    .map(lambda post: get_description_soup(post))
-    .map(lambda post: get_post_text(post)))
+  soup = get_description_soup(year_posts)
+  return get_post_text(soup)
